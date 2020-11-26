ec5b828dcb5c52960d665188866216949f04eab4
Mike B <mikeb999999@gmail.com>
2020-11-26 16:11:25 +0000

    Bring stuff within lazy loading boundary & use i/fs to strongly type the state

--- APM/src/app/products/state/product.reducer.ts
+++ APM/src/app/products/state/product.reducer.ts
@@ -1,5 +1,11 @@
 import { createReducer, on, createAction } from '@ngrx/store';
+import * as AppState from '../../state/app.state';
+
 import { Product } from '../product';
+
+export interface State extends AppState.State {
+  products: ProductState;
+}
 
 export interface ProductState {
   showProductCode: boolean;
@@ -7,10 +13,10 @@ export interface ProductState {
   products: Product[];
 }
 
-export const productReducer = createReducer(
-  { showProductCode: true },   // Initial state
+export const productReducer = createReducer<ProductState>(
+  { showProductCode: true } as ProductState,   // Initial state
   // Action
-  on(createAction('[Product] Toggle Product Code'), state => {
+  on(createAction('[Product] Toggle Product Code'), (state): ProductState => {
       console.log('Original state:' + JSON.stringify(state));
     return {
       ...state,     // spread copy of the state
--- APM/src/app/state/app.state.ts
+++ APM/src/app/state/app.state.ts
@@ -1,6 +1,3 @@
-import { ProductState } from '../products/state/product.reducer';
-
 export interface State {
-   // products: ProductState,   not within lazy loading boundary :(
     user: any;
   }
